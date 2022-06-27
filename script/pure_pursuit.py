f4a88c0a8b29d3c43e861172caf468a70de89126
shunsei-t <shunsei3t6@gmail.com>
2022-06-27 23:49:53 +0900

    pub marker

--- script/pure_pursuit.py
+++ script/pure_pursuit.py
@@ -3,60 +3,69 @@
 
 from __future__ import print_function
 import imp
+from re import X
 
 import rospy
 import roslib
+import tf
 import numpy as np
 from std_msgs.msg import Bool
 from visualization_msgs.msg import Marker, MarkerArray
 
-def line_create(num=0, x1=0, y1=0, z1=0, x2=0, y2=0, z2=0, r=0, g=0, b=0, a=1.0, scale=0.3):
+def line_create(num=0, x_=0, y_=0, z_=0, r_=1, g_=0, b_=0, a_=1.0, scale=0.1):
 
     marker_data = Marker()
     marker_data.header.frame_id = "map"
     marker_data.header.stamp = rospy.Time.now()
     marker_data.id = num
-    marker_data.action = Marker.SPHERE
-    marker_data.type = Marker.CUBE
+    marker_data.action = Marker.ADD
+    marker_data.type = Marker.SPHERE
+    marker_data.lifetime = rospy.Duration()
 
-    marker_data.scale.x = 0.1
-    marker_data.scale.y = 0.1
-    marker_data.scale.z = 0.1
-    marker_data.pose.position.x = 0
-    marker_data.pose.position.y = 0
-    marker_data.pose.position.z = 0
+    marker_data.scale.x = scale
+    marker_data.scale.y = scale
+    marker_data.scale.z = scale
+    marker_data.pose.position.x = x_
+    marker_data.pose.position.y = y_
+    marker_data.pose.position.z = z_
     marker_data.pose.orientation.x = 0
     marker_data.pose.orientation.y = 0
     marker_data.pose.orientation.z = 0
     marker_data.pose.orientation.w = 1
-    marker_data.color.g = 1.0
-    marker_data.color.b = 0.0
-    marker_data.color.a = 1.0
+    marker_data.color.r = r_
+    marker_data.color.g = g_
+    marker_data.color.b = b_
+    marker_data.color.a = a_
 
     return marker_data
 
 class Pursuit:
     def __init__(self):
         self.auto_flag = False
+        self.pose = [0.0]*3
+        self.listener = tf.TransformListener()
 
-        self.pub_map_marker = rospy.Publisher('path_marker', MarkerArray, queue_size=10)
-
+        self.pub_path = rospy.Publisher('path_marker', MarkerArray, queue_size=10)
         loot_dir = roslib.packages.get_pkg_dir("robocon_slam")
         self.path = np.loadtxt(loot_dir+"/path/path.csv", delimiter=',')
-        self.map_marker = MarkerArray()
+        self.path_marker = MarkerArray()
         for i in range(self.path.shape[0]):
-            self.map_marker.markers.append(line_create(i,
+            self.path_marker.markers.append(line_create(i,
                                                        self.path[i, 0],
                                                        self.path[i, 1],
                                                        0,
-                                                       self.path[i, 2],
-                                                       self.path[i, 3],
-                                                       0,
-                                                       1, 1, 1, 1
+                                                       1,0,0,1,
+                                                       0.1
                                                        ))
 
     def flag_CB(self, data):
         self.auto_flag = data.data
+        
+    def run_pursuit(self, trans, rot):
+        self.pose[0] = trans[0]
+        self.pose[1] = trans[1]
+        self.pose[2] = tf.transformations.euler_from_quaternion(rot)
+
 
 if __name__ == '__main__':
     try:
@@ -67,9 +76,17 @@ if __name__ == '__main__':
 
         rospy.Subscriber("auto_flag", Bool, mypursuit.flag_CB)
         print(mypursuit.path)
+
         try:
             while not rospy.is_shutdown():
-                
+                try:
+                    (trans, rot) = mypursuit.listener.lookupTransform('/base_link', 'map', rospy.Time(0))
+                except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                    continue
+
+                mypursuit.run_pursuit(trans, rot)
+
+                mypursuit.pub_path.publish(mypursuit.path_marker)
                 r.sleep()
         except KeyboardInterrupt:
             pass
