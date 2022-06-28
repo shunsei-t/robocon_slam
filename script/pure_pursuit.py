fa0e82770015c1fb69e1ea8bf79fe87931c6acec
shunsei-t <shunsei3t6@gmail.com>
2022-06-28 23:17:22 +0900

    add send_tf

--- script/pure_pursuit.py
+++ script/pure_pursuit.py
@@ -10,6 +10,7 @@ import roslib
 import tf
 import numpy as np
 from std_msgs.msg import Bool
+from geometry_msgs.msg import Twist
 from visualization_msgs.msg import Marker, MarkerArray
 
 def line_create(num=0, x_=0, y_=0, z_=0, r_=1, g_=0, b_=0, a_=1.0, scale=0.1):
@@ -43,20 +44,22 @@ class Pursuit:
     def __init__(self):
         self.auto_flag = False
         self.pose = [0.0]*3
+        self.vel = [0.0]*3
+        self.cmd_vel = Twist()
         self.listener = tf.TransformListener()
+        self.remarkable_point = [0.0]*2
+
+        #pure_pursuitのパラメータ
+        self.dlimit = 0.05
+        self.vel_gain = 5
+        self.max_vel = 1
 
         self.pub_path = rospy.Publisher('path_marker', MarkerArray, queue_size=10)
+        self.pub_vel = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
         loot_dir = roslib.packages.get_pkg_dir("robocon_slam")
         self.path = np.loadtxt(loot_dir+"/path/path.csv", delimiter=',')
         self.path_marker = MarkerArray()
-        for i in range(self.path.shape[0]):
-            self.path_marker.markers.append(line_create(i,
-                                                       self.path[i, 0],
-                                                       self.path[i, 1],
-                                                       0,
-                                                       1,0,0,1,
-                                                       0.1
-                                                       ))
 
     def flag_CB(self, data):
         self.auto_flag = data.data
@@ -65,6 +68,41 @@ class Pursuit:
         self.pose[0] = trans[0]
         self.pose[1] = trans[1]
         self.pose[2] = tf.transformations.euler_from_quaternion(rot)
+
+        d = (self.path[:, 0] - self.pose[0])**2 + (self.path[:, 1] - self.pose[1])**2
+        self.remarkable_point = np.argmin(d)
+
+        if self.remarkable_point <= len(self.path)-2:
+            self.vel[0] = self.vel_gain*(self.path[self.remarkable_point + 1, 0] - self.pose[0])
+            self.vel[1] = self.vel_gain*(self.path[self.remarkable_point + 1, 1] - self.pose[1])
+            if self.vel[0] > self.max_vel:
+                self.vel[0] = self.max_vel
+            if self.vel[1] > self.max_vel:
+                self.vel[1] = self.max_vel
+            self.cmd_vel.linear.x = self.vel[0]
+            self.cmd_vel.linear.y = self.vel[1]
+        
+        if self.auto_flag:
+            self.pub_vel.publish(self.cmd_vel)
+
+        for i in range(self.path.shape[0]):
+            if i==self.remarkable_point:
+                self.path_marker.markers.append(line_create(i,
+                                                    self.path[i, 0],
+                                                    self.path[i, 1],
+                                                    0,
+                                                    1,0,0,1,
+                                                    0.1
+                                                    ))
+            else:
+                self.path_marker.markers.append(line_create(i,
+                                                    self.path[i, 0],
+                                                    self.path[i, 1],
+                                                    0,
+                                                    0,1,0,1,
+                                                    0.1
+                                                    ))
+        self.pub_path.publish(self.path_marker)
 
 
 if __name__ == '__main__':
@@ -80,13 +118,10 @@ if __name__ == '__main__':
         try:
             while not rospy.is_shutdown():
                 try:
-                    (trans, rot) = mypursuit.listener.lookupTransform('/base_link', 'map', rospy.Time(0))
+                    (trans, rot) = mypursuit.listener.lookupTransform('map', 'base_link', rospy.Time(0))
+                    mypursuit.run_pursuit(trans, rot)
                 except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-                    continue
-
-                mypursuit.run_pursuit(trans, rot)
-
-                mypursuit.pub_path.publish(mypursuit.path_marker)
+                    pass
                 r.sleep()
         except KeyboardInterrupt:
             pass
---
+++ script/send_tf.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python  
+import rospy
+import tf
+
+if __name__ == '__main__':
+    rospy.init_node('my_tf_broadcaster')
+    br = tf.TransformBroadcaster()
+    rate = rospy.Rate(10.0)
+    while not rospy.is_shutdown():
+        br.sendTransform((2.5, 2.5, 0.0),
+                         (0.0, 0.0, 0.0, 1.0),
+                         rospy.Time.now(),
+                         "base_link",
+                         "map")
+        rate.sleep()
